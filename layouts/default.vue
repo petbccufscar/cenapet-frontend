513d481a42fadb6dc3cc3da826a8d9cab4f3c6f4
igorlmgx <igorlmgx@gmail.com>
2020-02-25 15:44:19 +0000

    Criada Navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
+    <TheNavbar />
     <nuxt />
   </div>
 </template>
 
+<script>
+import TheNavbar from "@/components/TheNavbar";
+export default {
+  components: {
+    TheNavbar
+  }
+};
+</script>
+
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
