8197ff683f0776f182cb2feb959ac48bef07eee0
igorlmgx <igorlmgx@gmail.com>
2020-06-06 16:18:21 +0000

    Alterando mais bootstrap

--- pages/sesu-mec.vue
+++ pages/sesu-mec.vue
@@ -1,90 +1,50 @@
 
 
 <template>
-    <b-container fluid >
+  <div class="container-fluid mb-4">
 
-        <b-row no-gutters class = "titulo" align-v = "center" >
-            
-            <b-col> 
-                <h1 class="texto-titulo"> Documentos da SESu/MEC </h1> 
-            </b-col>
-            
-        </b-row>
+    <div class="row no-gutters page-title align-content-center">
+      <div class="col">
+        <h1 class="text-center">Documentos da SESu/MEC</h1>
+      </div>
+    </div>
 
-        <b-container>
-            <b-row>
-                <b-col sm='12' class = "texto">
-                    <b-link href="#" enable>
-                            Lei 11.180 – 23 de setembro de 2005
-                    </b-link>
-                </b-col>
-            </b-row>
-            <b-row>
-                <b-col sm='12' class = "texto">
-                    <div>
-                        <b-link href="#" enable>
-                            Ofício Circular nº01-2015 – Preenchimento e envio da prestação de contas 2014 pelo tutor
-                        </b-link>
-                    </div>
-                </b-col>
-            </b-row>
-            <b-row>
-                <b-col sm='12' class = "texto">
-                    <div>
-                        <b-link href="#" enable>
-                            Ofício Circular nº 03 2015 MEC SESu DIPES CGRE
-                        </b-link>
-                    </div>
-                </b-col>
-            </b-row>
-            <b-row>
-                <b-col sm='12' class = "texto">
-                    <div>
-                        <b-link href="#" enable>
-                            Ofício Circular nº 04 2015 MEC SESu DIPES CGRE – calendário anual de planejamento, relatório e prestação de contas
-                        </b-link>
-                    </div>
-                </b-col>
-            </b-row>
-            <b-row>
-                <b-col sm='12' class = "texto">
-                    <div>
-                        <b-link href="#" enable>
-                            Passo a passo de como fazer sua solicitação no MEC-CUBE
-                        </b-link>
-                    </div>
-                </b-col>
-            </b-row>
-        </b-container>
-
-    </b-container>
-    
+    <div class="container">
+      <div class="row no-gutters">
+        <div class="col mt-4">
+          <h4>
+            <a href="#">Lei 11.180 – 23 de setembro de 2005</a>
+          </h4>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col mt-4">
+          <h4>
+            <a href="#">Ofício Circular nº01-2015 – Preenchimento e envio da prestação de contas 2014 pelo tutor</a>
+          </h4>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col mt-4">
+          <h4>
+            <a href="#">Ofício Circular nº 03 2015 MEC SESu DIPES CGRE</a>
+          </h4>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col mt-4">
+          <h4>
+            <a href="#">Ofício Circular nº 04 2015 MEC SESu DIPES CGRE – calendário anual de planejamento, relatório e prestação de contas</a>
+          </h4>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col mt-4">
+          <h4>
+            <a href="#">Passo a passo de como fazer sua solicitação no MEC-CUBE</a>
+          </h4>
+        </div>
+      </div>
+    </div>
+  </div>
 </template> 
-
-
-<style>
-.texto-titulo{
-    text-align:center;
-}
-
-.titulo{
-    height: 6rem;
-    width: 100%;
-    color:var(--accent);
-    background-color: var(--footer-dark);
-}
-
-.sub-titulo{
-    height: 7rem;
-    margin: 0.5rem;
-    text-align:center;
-}
-
-.texto{
-    
-    text-align:left;
-    font-size:1.5rem;
-    margin-top:4rem;
-}
-
-</style>
