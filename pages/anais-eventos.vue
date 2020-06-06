22da0398475d0ad9375d2ba2ebacd4fa49348b8f
igorlmgx <igorlmgx@gmail.com>
2020-06-06 16:18:21 +0000

    Alterando mais bootstrap

--- pages/anais-eventos.vue
+++ pages/anais-eventos.vue
@@ -1,39 +1,19 @@
 <template>
-  <b-container>
-    <b-container class = "intro">
-      <h1 class = "intro-title"> Anais de Eventos </h1>
-      <p class = "intro-text">
+  <div class="container-fluid mb-4">
+    <div class="row no-gutters page-title align-content-center">
+      <div class="col">
+        <h1 class="text-center">Anais de Eventos</h1>
+      </div>
+    </div>
+    <div class="container mt-4">
+      <h4>
         <b>ENAPET</b>
-        <br>
-        <br>
-         - <b-link href = "https://cenapet.files.wordpress.com/2020/03/anais_xxiv_encontro_nacional_dos_grupos_do_programa_de_educacao_tutorial_2019_compressed.pdf" > XXIV ENAPET | Natal - RN 2019 </b-link>
-         <br>
-         - <b-link href = "https://media.wix.com/ugd/0ffe63_c9214838cd33403cab75ac77ecb5abeb.pdf" > XXI ENAPET | Rio Branco - AC 2016 </b-link>
+      </h4>
+      <p class="mt-3">-
+        <a href="https://cenapet.files.wordpress.com/2020/03/anais_xxiv_encontro_nacional_dos_grupos_do_programa_de_educacao_tutorial_2019_compressed.pdf">XXIV ENAPET | Natal - RN 2019</a>
+        <br />-
+        <a href="https://media.wix.com/ugd/0ffe63_c9214838cd33403cab75ac77ecb5abeb.pdf">XXI ENAPET | Rio Branco - AC 2016</a>
       </p>
-    </b-container>
-  </b-container>
+    </div>
+  </div>
 </template>
-
-
-<style>
-.container-fluid {
-  padding: 0;
-}
-.intro {
-  margin-top: 1rem;
-  text-align: center;
-}
-.intro-title {
-  color: var(--accent);
-  margin: 3rem 0 3rem 0;
-  letter-spacing: 2px;
-  font-weight: 600;
-}
-.intro-text {
-  font-size: 1.2rem;
-  text-align: left;
-  letter-spacing: 1px;
-  line-height: 2rem;
-  margin: 1rem 8vw;
-}
-</style>
--- pages/documentos-conselhos.vue
+++ pages/documentos-conselhos.vue
@@ -1,78 +1,70 @@
 
 
 <template>
-    <b-container fluid >
-        <b-row no-gutters class = "titulo" align-v = "center">
-            <b-col> 
-                <h1 class="texto-titulo"> Documentos do Conselho </h1> 
-            </b-col>
-        </b-row>
+  <div class="container-fluid">
+    <div class="row no-gutters titulo align-items-center">
+      <div class="col">
+        <h1 class="texto-titulo">Documentos do Conselho</h1>
+      </div>
+    </div>
 
-        <b-container>
-            <b-row no-gutters>
-                <b-col sm='12'>
-                    <h3 class="sub-titulo">  Atas e demais documentos produzidos pelo Conselho da CENAPET</h3>
-                </b-col>
-            </b-row>
-            <b-row no-gutters >
-                <b-col sm='12' class = "texto">
-                    <div>
-                        <b-link href="1 Ata da 3a Reunião Ordinária do Conselho da CENAPET em 2015, realizada durante o ENAPET 2015.pdf" enable>
-                            Ata da 3a Reunião Ordinária do Conselho da CENAPET em 2015, realizada durante o ENAPET 2015
-                        </b-link>
-                    </div>
-                </b-col>
-            </b-row>
-            <b-row no-gutters >
-                <b-col sm='12' class = "texto">
-                    <div>
-                        <b-link href="2 Ata da 1a Reunião Ordinária do Conselho da CENAPET em 2015 realizada no dia 06022015" enable>
-                            Ata da 1a Reunião Ordinária do Conselho da CENAPET em 2015, realizada no dia 06/02/2015
-                        </b-link>
-                    </div>
-                </b-col>
-            </b-row>
-            <b-row no-gutters >
-                <b-col sm='12'>
-                    <h3 class="sub-titulo">Composição do Conselho</h3>
-                </b-col>
-            </b-row>
-            <b-row no-gutters >
-                <b-col sm='12' class = "texto">
-                    <div>
-                        <b-link href="2 Ata da 1a Reunião Ordinária do Conselho da CENAPET em 2015 realizada no dia 06022015" enable>
-                            Conselho CENAPET 2015 – 2016 
-                        </b-link>
-                    </div>
-                </b-col>
-            </b-row>
-        </b-container>
-    </b-container>
-    
+    <div class="container">
+      <div class="row no-gutters">
+        <div class="col-sm-12">
+          <h3 class="sub-titulo">Atas e demais documentos produzidos pelo Conselho da CENAPET</h3>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col-sm-12 texto">
+          <a
+            href="1 Ata da 3a Reunião Ordinária do Conselho da CENAPET em 2015, realizada durante o ENAPET 2015.pdf"
+          >Ata da 3a Reunião Ordinária do Conselho da CENAPET em 2015, realizada durante o ENAPET 2015</a>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col-sm-12 texto">
+          <a
+            href="2 Ata da 1a Reunião Ordinária do Conselho da CENAPET em 2015 realizada no dia 06022015"
+          >Ata da 1a Reunião Ordinária do Conselho da CENAPET em 2015, realizada no dia 06/02/2015</a>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col-sm-12">
+          <h3 class="sub-titulo">Composição do Conselho</h3>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col-sm-12 texto">
+          <a
+            href="2 Ata da 1a Reunião Ordinária do Conselho da CENAPET em 2015 realizada no dia 06022015"
+          >Conselho CENAPET 2015 – 2016</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template> 
 
 
 <style>
-.texto-titulo{
-    text-align:center;
+.texto-titulo {
+  text-align: center;
 }
 
-.titulo{
-    height: 6rem;
-    width: 100%;
-    color:var(--accent);
-    background-color: var(--footer-dark);
+.titulo {
+  height: 6rem;
+  width: 100%;
+  color: var(--accent);
+  background-color: var(--footer-dark);
 }
 
-.sub-titulo{
-    margin-top:4rem;
-    text-align:left;
+.sub-titulo {
+  margin-top: 4rem;
+  text-align: left;
 }
 
-.texto{
-    text-align:left;
-    font-size:1.5rem;
-    margin-top:4rem;
+.texto {
+  text-align: left;
+  font-size: 1.5rem;
+  margin-top: 4rem;
 }
-
 </style>
