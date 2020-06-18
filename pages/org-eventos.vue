18f09d3efb15fc471a060fe019bbc200281d3ec4
igorlmgx <igorlmgx@gmail.com>
2020-06-18 20:46:25 +0000

    Adicionando rotas e páginas

--- pages/org-eventos.vue
+++ pages/org-eventos.vue
@@ -6,35 +6,7 @@
       </div>
     </div>
     <div class="row no-gutters text">
-      <div class="col">
-        <h5>PETianas e PETianos,</h5>
-        <p>A Comissão Executiva Nacional do PET (CENAPET), Mobilização Nacional (gestão 2016-2018), cumprimenta cada uma das Organizações Regionais e a Organização Nacional, demonstrando abertura e disposição para, que no ano de 2018 o Programa de Educação Tutorial possa ser aperfeiçoado de forma dialogada e coletiva por meio dos Encontros – Regionais e Nacional.</p>
-        <p>Nesse intuito, disponibilizamos documentos orientadores para a organização e realização dos Encontros Regionais e Encontro Nacional de modo a seguir a estrutura do Programa de Educação Tutorial e deliberações nos Encontros anteriores. Todavia, indicamos a necessidade que as diretrizes sejam levadas em consideração para melhor deliberação das atividades e garantia da representatividade PETiana a nível regional e nacional.</p>
-        <h5>Documentos importantes e norteadores:</h5>
-        <p>
-          <b>1) <a href="https://drive.google.com/file/d/10ltQCLmy1maUXllPWNyGBH0dtblG4kZC/view">Diretrizes para a organização dos Encontros (Regionais e Nacional)</a></b>: documento com as instruções para realização das atividades necessárias em todos os Encontros, contendo todas as diretrizes e prazos;
-        </p>
-        <p>
-          <b>2) Modelos</b>:
-          <br />a) <a href="https://drive.google.com/file/d/1IrvQx-6-AEuBhHRnb0a0Xs5EAwdQPntF/view">Modelo de Regimento do Encontro (Estrutura);</a>
-          <br />b) <a href="https://drive.google.com/file/d/1uuP9fq5xA9VY76j3XHhUeq-d66vhQJjz/view">Modelo de Regimento e do Relatório do GDT;</a>
-          <br />c) <a href="https://drive.google.com/file/d/1-gzOHYQ1yDnpM54nD6HJf9lDNxBNZJ3N/view">Modelo para envio da Composição da Comissão de Avaliação Nacional (Nacional);</a>
-          <br />d) <a href="https://drive.google.com/file/d/1MQBkcblA-i3nvaH4I-UhvtiBRRnIRyzx/view">Modelo de Regimento e da Ata da Assembleia Final (regionais)/Geral (ENAPET);</a>
-          <br />e) <a href="https://drive.google.com/file/d/1MM2JCEOFFZE8ACjButJARiKuAzEEEXON/view">Modelo de Relatório da Assembleia Final (Regionais)/Geral (ENAPET);</a>
-        </p>
-        <p>
-          <b>
-            3) <a href="https://drive.google.com/file/d/1-pr4rb7cBTwF6wmRIlKya--OFl8TbGzP/view">Estatuto da CENAPET</a></b>: este é o Estatuto da CENAPET atualizado. Contém as modificações geradas no XX ENAPET, Brasília, portanto, sigam esta versão;
-        </p>
-        <p>
-          <b>4) </b> Ainda, o <b><a href="https://drive.google.com/file/d/1iAuFZTg1__fIy2qUnoqgyqje1K_D0Lde/view">EDITAL</a></b> para ELEIÇÃO DA DIRETORIA DA CENAPET – GESTÃO 2018/2020.
-        </p>
-        <h5>Vamos unir esforços e trabalhar coletivamente!</h5>
-        <p>
-          Diretoria da CENAPET
-          <br />Mobilização Nacional (gestão 2016-2018)
-        </p>
-      </div>
+      <div v-html="$md.render(organizacao.conteudo)"></div>
     </div>
     <div class="row no-gutters justify-content-center my-4">
       <div class="col-md-6 text-center">
@@ -61,3 +33,15 @@ iframe {
   max-height: 50vh;
 }
 </style>
+
+<script>
+import axios from "axios";
+
+export default {
+  asyncData({ params }) {
+    return axios.get(`http://localhost:1337/organizacao`).then(res => {
+      return { organizacao: res.data };
+    });
+  }
+};
+</script>
