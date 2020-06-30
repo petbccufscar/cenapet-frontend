792543a6dc570e7af6e35512cbf8dfc52ea088c3
igorlmgx <igorlmgx@gmail.com>
2020-06-30 00:34:20 +0000

    NoticiaDeck com backend

--- components/NoticiaCard.vue
+++ components/NoticiaCard.vue
@@ -10,7 +10,7 @@
             <h4 class="card-title">{{ titulo }}</h4>
           </nuxt-link>
           <p class="card-text">{{ unformat(conteudo) }}</p>
-          <p class="card-text text-muted mt-1">Publicada em {{ data_publicacao }}</p>
+          <p class="card-text text-muted mt-1">Publicada em {{ moment(data_publicacao).format("DD/MM/YYYY") }}</p>
         </div>
       </div>
     </div>
@@ -38,8 +38,8 @@
   display: -webkit-box;
   -webkit-box-orient: vertical;
   -webkit-line-clamp: 4; /* number of lines to show */
-  line-height: 1.3rem; /* fallback */
-  max-height: calc(1.3rem * 4); /* fallback */
+  line-height: 1.7rem; /* fallback */
+  max-height: calc(1.7rem * 4); /* fallback */
 }
 
 .card-text {
@@ -72,7 +72,14 @@
 </style>
 
 <script>
+var moment = require("moment");
+
 export default {
+  data() {
+    return {
+      moment: moment     
+    }
+  },
   methods: {
     unformat(text) {
       const removeMd = require("remove-markdown");
