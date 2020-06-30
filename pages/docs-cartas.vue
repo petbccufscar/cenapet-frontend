011616da54495c665e601198f18648b0bc6e62ed
Mayk Bezerra <38228656+mayktu@users.noreply.github.com>
2020-06-30 17:00:38 -0300

    Update docs-cartas.vue

--- pages/docs-cartas.vue
+++ pages/docs-cartas.vue
@@ -15,36 +15,24 @@
       </div>
       <div class="row no-gutters mt-3">
         <div class="col">
-          <p><a href="https://drive.google.com/open?id=1kN5aW8ijFvIpAcQ3cjsYdfPfjRjFCv5V" target="_blank">Carta ao Coordenador Geral de Relações Estudantis Vicente</a></p>
-          <p><a href="https://drive.google.com/open?id=1f7fGyvSfVdM7VWCNQxFyjn-rD36AkCnd" target="_blank">Carta ao Reitor da UFAC</a></p>
-          <p><a href="https://drive.google.com/open?id=1g3AN1D9l1NWGckjSKup5NCp-iiiFfwM6" target="_blank">Carta ao Ministro da Educação Mercadante</a></p>
-          <p><a href="https://drive.google.com/open?id=1wzvSvp6W0XIa0A7PqRSsa77PXLVPuETp" target="_blank">Carta a Presidenta da Andifes</a></p>
-          <p><a href="https://drive.google.com/open?id=16PkyjghPPhup36JGgpkvhuNf0a5vwrc1" target="_blank">Carta a Presidenta do Brasil</a></p>
-          <p><a href="https://drive.google.com/open?id=1abXycfgD61vQvZhd3FOjAKqWE3tjUfyv" target="_blank">Carta ao Coordenador Geral de Relações Estudantis</a></p>
-          <p><a href="https://drive.google.com/open?id=1Vl6jf8cYeGtaDdkYl0LhO_VvGYwtnSL5" target="_blank">Carta de Apoio – UDESC</a></p>
-          <p><a href="https://drive.google.com/open?id=1ecd7_r0XJW5ewA9ae-mAdpGUg7PFoJnS" target="_blank">Carta do XVI Sudeste PET 2016</a></p>
-          <p><a href="https://drive.google.com/open?id=1tYD_XlUPZ4jRQO6wDXYH5SGETE36tF89" target="_blank">Carta sobre o Tempo de Tutoria</a></p>
-          <p><a href="https://drive.google.com/open?id=13vz7xthXIxC5MOpXzPJMbcibrT1XPKJC" target="_blank">Carta enviada pela diretoria da CENAPET ao Secretário de Educação no 08/07/2015</a></p>
-          <p><a href="https://drive.google.com/open?id=1Y_sKsX4HbTGNgcV0jLZoxMzF13yEfFno" target="_blank">Carta encaminhada ao Secretário de Educação em 16.4.2015</a></p>
-          <p><a href="https://drive.google.com/open?id=1CNXdEltWTj0HhMO-6cHU2swI_8fkHqjT" target="_blank">Carta de Ouro Preto enviada pela CENAPET em 2015</a></p>
-          <p><a href="https://drive.google.com/open?id=1kVBUtuPpDCfG7YlG37oNQG6tPJf1PJOo" target="_blank">Carta aos Petianos: Por uma gestão de responsabilidade e construção</a></p>
-          <p><a href="https://drive.google.com/open?id=1aW7-_AMss74D2Z12bxa6sFgUit-kQrSS" target="_blank">Carta SULPET 2013</a></p>
-          <p><a href="https://drive.google.com/open?id=1fwn4Uq7Bgmjq_ecmmzL0Bm3RSCg4fJNo" target="_blank">Carta Sudeste PET 2012</a></p>
-          <p><a href="https://drive.google.com/open?id=16t2WaTZBUGW3eKUM10Rn6DhlQEWSM21o" target="_blank">Carta Sudeste PET 2013</a></p>
-          <p><a href="https://drive.google.com/open?id=1RWpxLRz2UYj0bfDUbFqGX0_fgQLc-Mn3" target="_blank">Carta ECONPET 2013</a></p>
-          <p><a href="https://drive.google.com/open?id=1MbsSC3mepmMZG-a3G12TnVdY57BlaK2h" target="_blank">Carta ENEPET 2013</a></p>
-          <p><a href="https://drive.google.com/open?id=1mDYiXmYQvnIDB4-8H9aspY3H9ToTGkHN" target="_blank">Carta CENAPET 2014</a></p>
-          <p><a href="https://drive.google.com/open?id=1SOUoEVBGklFaMt63aUKFBWNymc-DQ1pB" target="_blank">Carta CENAPET ao Senador Inácio Arruda</a></p>
-          <p><a href="https://drive.google.com/open?id=1oDW5MJv2g00ecEP5bKo_oj3k4qsJcHzR" target="_blank">Carta CENAPET ao Secretario Luiz Claudio 1</a></p>
-          <p><a href="https://drive.google.com/open?id=1BzEyLdd0Mbt9X6OU9FPBKMXVQzpIvUiq" target="_blank">Carta CENAPET ao Secretario Luiz Claudio 2</a></p>
-          <p><a href="https://drive.google.com/open?id=1hmHwzIN3nPTHJSpiBDmn4__c4D2ggpim" target="_blank">Carta CENAPET ao Secretário Paulo Speller 1</a></p>
-          <p><a href="https://drive.google.com/open?id=1ntzhIVTyJxXQclIjhT6_9mghvgcaEcv7" target="_blank">Carta CENAPET ao Secretário Paulo Speller 2</a></p>
-          <p><a href="https://drive.google.com/open?id=1mLS16BrY-dXLNAfOUxdAoCBgU6KYNyZp" target="_blank">Carta CENAPET ao Secretário Paulo Speller 3</a></p>
-          <p><a href="https://drive.google.com/open?id=10EzszcNPYwfFz4ZN3VsNeOYV5NoZiIX9" target="_blank">Carta CENAPET ao Secretário Amaro Pessoa 1</a></p>
-          <p><a href="https://drive.google.com/open?id=1YIsIWddnE8RzydZ_e90tubjT84CkpobL" target="_blank">Carta CENAPET ao Secretário Amaro Pessoa 2</a></p>
-          <p><a href="https://drive.google.com/open?id=11TKO3UQRFtkJZvPzfcsrP_Ow22qm32ZA" target="_blank">Carta CENAPET ao Secretário Amaro Pessoa 3</a></p>
+
+          <div v-html="$md.render(carta.conteudo)"></div>
+
         </div>
       </div>
     </div>
   </div>
 </template>
+
+
+<script>
+import axios from "axios"
+
+export default {
+  asyncData({ params }) {
+    return axios.get(process.env.baseURL + `/cartas`).then(res => {
+      return { carta: res.data};
+    });
+  }
+};
+</script>
