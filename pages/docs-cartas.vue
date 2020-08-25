15d178ef797a5658f8f59b438c41dda2fd29ab88
Mayk Bezerra <38228656+mayktu@users.noreply.github.com>
2020-08-25 20:22:33 -0300

    Update docs-cartas.vue

--- pages/docs-cartas.vue
+++ pages/docs-cartas.vue
@@ -8,13 +8,6 @@
     <div class="container">
       <div class="row no-gutters mt-3">
         <div class="col">
-          <h4>
-            Descrição bacanuda de cartas: Cartas referente ao papapapapapapap ipsum ipsum ipsum ipsão pipipi popopo
-          </h4>
-        </div>
-      </div>
-      <div class="row no-gutters mt-3">
-        <div class="col">
 
           <div v-html="$md.render(carta.conteudo)"></div>
 
