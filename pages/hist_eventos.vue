b932c6fec67dc1bd8d1abf71d6c593bdba14ab2b
amandapmn <40667726+amandapmn@users.noreply.github.com>
2020-10-02 09:09:53 -0300

    Pag historico de eventos
    Para substituir a pagina 'eventos.vue'

---
+++ pages/hist_eventos.vue
@@ -0,0 +1,238 @@
+<template>
+  <div class="container-fluid min-70 mb-4">
+    <div class="row no-gutters align-items-center page-title">
+      <div class="col">
+        <h1 class="text-center">Historico de Eventos</h1>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row no-gutters justify-content-around align-items-center">
+        <div class="col-12 my-4 ano">
+          <h2>ENAPET</h2>
+        </div>
+        <div
+          class="col-sm-12 col-md-8 col-lg-5 my-3"
+          v-for="evento in enapets"
+          :key="evento.id"
+        >
+          <EventoCard
+            :id="evento.id"
+            :logo="getImgUrl(evento.logo)"
+            :titulo="evento.titulo"
+            :local="evento.local"
+            :data_inicio="evento.data_inicio"
+            :data_fim="evento.data_fim"
+            :links="evento.links"
+          />
+        </div>
+      </div>
+
+      <div
+        class="row no-gutters justify-content-around align-items-center"
+      >
+        <div class="col-12 my-4 ano">
+          <h2>ENEPET</h2>
+        </div>
+        <div
+          class="col-sm-12 col-md-8 col-lg-5 my-3"
+          v-for="evento in enepets"
+          :key="evento.id"
+        >
+          <EventoCard
+            :id="evento.id"
+            :logo="getImgUrl(evento.logo)"
+            :titulo="evento.titulo"
+            :local="evento.local"
+            :data_inicio="evento.data_inicio"
+            :data_fim="evento.data_fim"
+            :links="evento.links"
+          />
+        </div>
+      </div>
+
+      <div
+        class="row no-gutters justify-content-around align-items-center"
+      >
+        <div class="col-12 my-4 ano">
+          <h2>ECONPET</h2>
+        </div>
+        <div
+          class="col-sm-12 col-md-8 col-lg-5 my-3"
+          v-for="evento in econpets"
+          :key="evento.id"
+        >
+          <EventoCard
+            :id="evento.id"
+            :logo="getImgUrl(evento.logo)"
+            :titulo="evento.titulo"
+            :local="evento.local"
+            :data_inicio="evento.data_inicio"
+            :data_fim="evento.data_fim"
+            :links="evento.links"
+          />
+        </div>
+      </div>
+
+      <div
+        class="row no-gutters justify-content-around align-items-center"
+      >
+        <div class="col-12 my-4 ano">
+          <h2>ECOPET</h2>
+        </div>
+        <div
+          class="col-sm-12 col-md-8 col-lg-5 my-3"
+          v-for="evento in ecopets"
+          :key="evento.id"
+        >
+          <EventoCard
+            :id="evento.id"
+            :logo="getImgUrl(evento.logo)"
+            :titulo="evento.titulo"
+            :local="evento.local"
+            :data_inicio="evento.data_inicio"
+            :data_fim="evento.data_fim"
+            :links="evento.links"
+          />
+        </div>
+      </div>
+
+      <div
+        class="row no-gutters justify-content-around align-items-center"
+      >
+        <div class="col-12 my-4 ano">
+          <h2>NORTEPET</h2>
+        </div>
+        <div
+          class="col-sm-12 col-md-8 col-lg-5 my-3"
+          v-for="evento in nortepets"
+          :key="evento.id"
+        >
+          <EventoCard
+            :id="evento.id"
+            :logo="getImgUrl(evento.logo)"
+            :titulo="evento.titulo"
+            :local="evento.local"
+            :data_inicio="evento.data_inicio"
+            :data_fim="evento.data_fim"
+            :links="evento.links"
+          />
+        </div>
+      </div>
+
+      <div
+        class="row no-gutters justify-content-around align-items-center"
+      >
+        <div class="col-12 my-4 ano">
+          <h2>SUDESTEPET</h2>
+        </div>
+        <div
+          class="col-sm-12 col-md-8 col-lg-5 my-3"
+          v-for="evento in sudestepets"
+          :key="evento.id"
+        >
+          <EventoCard
+            :id="evento.id"
+            :logo="getImgUrl(evento.logo)"
+            :titulo="evento.titulo"
+            :local="evento.local"
+            :data_inicio="evento.data_inicio"
+            :data_fim="evento.data_fim"
+            :links="evento.links"
+          />
+        </div>
+      </div>
+
+      <div
+        class="row no-gutters justify-content-around align-items-center"
+      >
+        <div class="col-12 my-4 ano">
+          <h2>SULPET</h2>
+        </div>
+        <div
+          class="col-sm-12 col-md-8 col-lg-5 my-3"
+          v-for="evento in sulpets"
+          :key="evento.id"
+        >
+          <EventoCard
+            :id="evento.id"
+            :logo="getImgUrl(evento.logo)"
+            :titulo="evento.titulo"
+            :local="evento.local"
+            :data_inicio="evento.data_inicio"
+            :data_fim="evento.data_fim"
+            :links="evento.links"
+          />
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style>
+.ano {
+  color: var(--accent);
+  letter-spacing: 2px;
+  text-align: center;
+}
+</style>
+
+<script>
+import EventoCard from "@/components/EventoCard.vue";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      evento: [],
+      
+    };
+  },
+
+  components: {
+    "EventoCard": EventoCard,
+  },
+
+  methods: {
+    getImgUrl: function (img) {
+      return img
+        ? process.env.baseURL + img.url
+        : require("~/assets/images/logo_escuro.png");
+    },
+  },
+  async asyncData({ params }) {
+    const enapets = await axios.get(
+      process.env.baseURL + "/Hist-eventos?titulo_contains=ENAPET"
+    );
+    const enepets = await axios.get(
+    process.env.baseURL + "/Hist-eventos?titulo_contains=ENEPET"
+    );
+    const econpets = await axios.get(
+      process.env.baseURL + "/Hist-eventos?titulo_contains=ECONPET"
+    );
+    const ecopets = await axios.get(
+      process.env.baseURL + "/Hist-eventos?titulo_contains=ECOPET"
+    );
+    const nortepets = await axios.get(
+      process.env.baseURL + "/Hist-eventos?titulo_contains=NORTEPET"
+    );
+    const sudestepets = await axios.get(
+      process.env.baseURL + "/Hist-eventos?titulo_contains=SUDESTEPET"
+    );
+    const sulpets = await axios.get(
+      process.env.baseURL + "/Hist-eventos?titulo_contains=SULPET"
+    );
+    return { 
+      enapets: enapets.data,
+      enepets: enepets.data,
+      econpets: econpets.data,
+      ecopets: ecopets.data,
+      nortepets: nortepets.data,
+      sudestepets: sudestepets.data,
+      sulpets: sulpets.data,
+
+       };
+  },
+}
+</script>
