4d79ad1df8f920709f8f339ee779358f02785593
amandapmn <40667726+amandapmn@users.noreply.github.com>
2020-11-12 00:24:24 -0300

    Adicionando algumas páginas
    Eventos, sobre o PET, docs do SESu, relatórios, entre outros

---
+++ pages/ecopet.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container-fluid min-70 mb-4">
+    <div class="row no-gutters align-items-center page-title">
+      <div class="col">
+        <h1 class="text-center">ECOPET</h1>
+      </div>
+    </div>
+    <div class="container">
+        <div class="card">
+            <div
+              class="col-sm-12 col-md-8 col-lg-6 my-3"
+              v-for="evento in ecopets"
+              :key="evento.id"
+            >
+                <EventoCard
+                  :id="evento.id"
+                  :logo="getImgUrl(evento.logo)"
+                  :titulo="evento.titulo"
+                  :local="evento.local"
+                  :data_inicio="evento.data_inicio"
+                  :data_fim="evento.data_fim"
+                  :links="evento.links"
+                />
+            </div>
+          </div>
+        </div>
+      </div>
+</template>
+
+<style scoped>
+.card-header {
+  padding: 1rem 0;
+}
+
+.card-header h2 {
+  color: var(--text-grey-darker);
+}
+
+.ano {
+  color: var(--accent);
+  letter-spacing: 2px;
+  text-align: center;
+}
+</style>
+
+<script>
+import EventoCard from "@/components/EventoCard.vue";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      evento: [],
+    };
+  },
+
+  components: {
+    EventoCard: EventoCard,
+  },
+
+  methods: {
+    getImgUrl: function (img) {
+      return img
+        ? process.env.baseURL + img.url
+        : require("~/assets/images/logo_escuro.png");
+    },
+  },
+  async asyncData({ params }) {
+    const ecopets = await axios.get(
+      process.env.baseURL + "/Hist-eventos?titulo_contains=ecopet"
+    );
+    return {
+      ecopets: ecopets.data,
+    };
+  },
+};
+</script>
---
+++ pages/sobre-pet.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container-fluid min-75 mb-4">
+    <div class="row no-gutters align-items-center page-title">
+      <div class="col">
+        <h1 class="text-center">O que é?</h1>
+      </div>
+    </div>
+    <div class="container mt-3">
+      <DynamicZone
+        v-for="content in this.sobrePet.conteudo"
+        :key="content.id"
+        :content="content"
+        class="my-3"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import DynamicZone from "@/components/DynamicZone.vue";
+
+export default {
+  head() {
+      return {
+        title: "O que é?",
+      }
+  },
+  components: {
+    DynamicZone
+  },
+  asyncData({ params }) {
+    axios.defaults.headers.post["Content-Type"] =
+      "application/x-www-form-urlencoded";
+    return axios.get(process.env.baseURL + `/sobre-pet`).then(res => {
+      return { sobrePet: res.data };
+    });
+  }
+};
+</script>
