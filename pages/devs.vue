361e075043147d16976c22358dd146a1ca9f2e78
donderileo <donderileo@gmail.com>
2021-03-21 21:44:24 -0300

    /devs

---
+++ pages/devs.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="mt-3 row">
+    <div class="col-md">
+        <div class="logo">
+            <img src="~/assets/images/LogoPETBCC.svg" alt="Logo PetBCC">
+        </div>
+        <div class="nome-pet">
+            PETBCC UFSCar
+        </div>
+        <div class="texto">
+            Esse site foi desenvolvido pelo PET-BCC da Universidade Federal de São Carlos, com objetivo de auxiliar a CENAPET na divulgação de suas comissões, eventos, documentos e notícias.
+        </div>
+    </div>
+    <div class="col-md">
+        <div class="tecs">
+            <img src="" alt="Nuxt">
+            <img src="" alt="BootStrap">
+        </div> 
+
+        <div class="infos-pet">
+            <div class="ativos">
+                <p>Conheça todos nossos projetos ativos</p>
+                <a href="https://petbcc.ufscar.br/projetos/"></a>
+            </div>
+            <div class="redes">
+                <p>Nos siga e acompanhe nosso trabalho nas Redes Sociais!</p>
+               
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<style scoped>
+
+.logo{
+    display:flex;
+    justify-content:center;
+}
+.logo img{
+    width:100%;
+    height:5rem;
+}
+
+</style>
